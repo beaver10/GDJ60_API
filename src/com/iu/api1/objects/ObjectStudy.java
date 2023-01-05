5b46daa219baceb86564d518857f763686a44642
hayoung jang <wkdgkdud1005@gmail.com>
2023-01-05 14:35:45 +0900

    1

---
+++ src/com/iu/api1/objects/ObjectStudy.java
@@ -0,0 +1,18 @@
+package com.iu.api1.objects;
+
+public class ObjectStudy {
+
+	public static void main(String[] args) {
+		//객체 생성 공식
+		Object object = new Object();
+		
+		
+		//멤버 사용(접근)
+		//참조변수명.변수명, 참조변수명,메서드명()
+		int result = object.hashCode();
+		System.out.println(result);
+		
+
+	}
+
+}
